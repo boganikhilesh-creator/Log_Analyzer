fc304a283f7dcb7098bbb94e1e599859ac29f2d2
boganikhilesh-creator <boganikhilesh@gmail.com>
2026-03-28 20:24:39 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,8 +1,10 @@
 import smtplib
+import os
 
-sender = "boganikhilesh@gmail.com"
+sender = "your_email@gmail.com"
+receiver = "your_email@gmail.com"
+password = os.getenv("EMAIL_PASS")
 receiver = "boganikhilesh08@gmail.com"
-password = "boganikhilesh@gmail.com"
 
 message = "Subject: ALERT\n\nSuspicious login activity detected!"
 
